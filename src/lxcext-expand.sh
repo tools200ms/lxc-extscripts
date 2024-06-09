6c6ee3d04f1307a737b386d8f025c6e1b4d49bf1
Barnaba Mateusz <barnaba@200ms.net>
2024-06-09 20:23:31 +0200

    Fix

--- src/lxcext-expand.sh
+++ src/lxcext-expand.sh
@@ -15,12 +15,12 @@ function op_expand() {
   cname=$1
   size_incr=$2
 
-  if [ -z "$cname" ] && ! [[ $size_incr =~ ^[0-9]{1,6}[k|m|g]$ ]]; then
+  if [ -z "$cname" ] || ! [[ $size_incr =~ ^[0-9]{1,6}[k|m|g]$ ]]; then
     echo "Provide container name and size by which to increase space"
     exit 1
   fi
 
-  if ! [ $(lxc-ls --stopped --line | grep -Fx cname$) ]; then
+  if ! [ $(lxc-ls --stopped --line | grep -Fx "$cname") ]; then
     echo "Container: $cname has not been stoped"
     exit 1
   fi
