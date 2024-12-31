de564d7ae093959366e86e21141ee612c3e58876
Mateusz Piwek <barnaba@200ms.net>
2024-12-31 18:56:54 +0100

    Int. tests progress

--- tests/dmki-gen-extract.py
+++ tests/dmki-gen-extract.py
@@ -4,7 +4,7 @@ import sys
 from lxc_extrpy import *
 
 class ExtrPy(unittest.TestCase):
-    def test_extr_DKMI(self):v
+    def test_extr_DKMI(self):
         with open('./tests/selector-test.txt') as file:
             selector, value, comment = extractDKMITxt(file.read())
 
